87d7039642694a70ef071fb88a08b70341951f5f
yaminiu <yniu@live.com.au>
2023-06-09 08:36:41 +1000

    update minigrep

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     println!("Searching {} ...", config.query);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
